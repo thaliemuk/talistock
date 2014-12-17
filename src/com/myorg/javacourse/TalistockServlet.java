2ea9401255aa18091aa413105a71f644461b1b7b
thalie <thaliemuk@gmail.com>
2014-12-17 20:17:55 +0200

    111

--- src/com/myorg/javacourse/TalistockServlet.java
+++ src/com/myorg/javacourse/TalistockServlet.java
@@ -5,7 +5,10 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class TalistockServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		int num1 = 4, num2 = 3, num3 = 7;
+		
+		
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Tali");
+		resp.getWriter().println("Result of:" + "(("+num1+" + "+num2+") * " + num3+") = "+ ((num1+num2) * num3));
 	}
 }
